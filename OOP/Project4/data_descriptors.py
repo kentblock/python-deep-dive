26c905565fb7a54222a9c87ce40f21381bc879ac
Riley Peel <rpeel4@outlook.com>
2020-05-18 20:19:27 -0700

    refactored and created baseevalidator class

--- OOP/Project4/data_descriptors.py
+++ OOP/Project4/data_descriptors.py
@@ -1,7 +1,7 @@
 from weakref import WeakKeyDictionary, ref
-#TODO refactor and add BaseValidator class
 
-class IntegerField:
+
+class BaseValidator:
 
     def __init__(self, min, max):
         self.min = min
@@ -14,45 +14,50 @@ class IntegerField:
         return self
     
     def __set__(self, instance, value):
+        self.validate(value)
+        self.values[id(instance)] = (ref(instance, self._remove_object), value)
+
+    def validate(self, value):
+        pass
+
+    def _remove_object(self, weak_ref):
+        for key in self.values.keys():
+            if self.values[key][0] is weak_ref:
+                del self.values[key]
+    
+
+class IntegerField(BaseValidator):
+
+    def __init__(self, min=None, max=None):
+        super().__init__(min, max)
+
+    def validate(self, value):
         if not isinstance(value, int):
-            raise TypeError("value must be of type str")
-        if value > self.max or value < self.min:
-            raise ValueError(f"Integer value must be greater than {self.min - 1} and less than {self.max + 1}")
-        print(f"instance {instance}, value {value}")
-        self.values[id(instance)] = (ref(instance, self._remove_object), value)
+            raise TypeError("value must be of type int")
+        if self.max: 
+            if value > self.max:
+                raise ValueError(f"Integer value must be less than {self.max + 1}")
+        if self.min:
+            if value < self.min:
+                raise ValueError(f"Integer value must be greater than {self.min - 1}")          
 
-    def _remove_object(self, weak_ref):
-        for key in self.values.keys():
-            if self.values[key][0] is weak_ref:
-                del self.values[key]
-                
-class CharField:
+class CharField(BaseValidator):
 
-    def __init__(self, min_len, max_len):
-        self.min_len = min_len
-        self.max_len = max_len
-        self.values = {}
+    def __init__(self, min_len=0, max_len=None):
+        super().__init__(min_len, max_len)
 
-    def __get__(self, instance, owner_class):
-        if instance:
-            return self.values[id(instance)]
-        return self
-
-    def __set__(self, instance, value):
+    def validate(self, value):
         if not isinstance(value, str):
-            raise TypeError("value must be of type str")
-        value = value.strip()
-        if len(value) > self.max_len or len(value) < self.min_len:
-            raise ValueError(f"String length must be greater than {self.min_len - 1} and less than {self.max_len + 1}")
-        self.values[id(instance)] = (ref(instance, self._remove_object), value)
-
-    def _remove_object(self, weak_ref):
-        for key in self.values.keys():
-            if self.values[key][0] is weak_ref:
-                del self.values[key]
-
+            raise TypeError("value must be of type string")
+        if self.max: 
+            if len(value) > self.max:
+                raise ValueError(f"String length must be less than {self.max + 1}")
+        if self.min:
+            if len(value) < self.min:
+                raise ValueError(f"String length must be greater than {self.min - 1}") 
 
 class Tester:
+
     __slots__ = '__weakref__'
     integer = IntegerField(1, 10)
     char = CharField(0, 200)
